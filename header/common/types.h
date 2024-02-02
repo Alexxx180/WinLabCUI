a917732ef60e1298019c8e28bbd758b97268bb2d
Alexxx180 <alexx120702@gmail.com>
2024-02-02 21:56:33 +0300

    Menu item concept completed

--- header/common/types.h
+++ header/common/types.h
@@ -17,6 +17,8 @@ struct Frame {
 struct Vector2s { short X, Y; };
 struct Vector2u { unsigned int X, Y; };
 
+struct Vector2c { unsigned char X, Y; };
+
 struct Vector2i { int X, Y; };
 
 struct Vector3d { double X, Y, Z; };
--- header/screen/art/controls/menu/menuitem.h
+++ header/screen/art/controls/menu/menuitem.h
@@ -3,72 +3,140 @@
 
 #include <vector>
 
+#include <windows.h>
+
+#include "common/types.h"
+
 class MenuItem {
     private:
-       
+        void (*m_command)() = NULL, (*m_back)() = NULL;
+        std::vector<MenuItem>* m_items = NULL;
+        Vector2c m_orientation, m_position;
+        unsigned char m_selection;
+        std::string m_caption;
 
-        void (*m_command)() = NULL;
+        void SetSelection(short next) {
+            m_selection = next;
+            m_items[m_selection].Focus();
+        }
+
+        void Minimize() {
+            short size = m_items.size();
+            while (--size > 0) {
+                m_items[size].Focus();
+                m_items[size].Clear();
+            }
+        }
 
         void Expand() {
-            
+            short size = m_items.size();
+            while (--size > 0) {
+                m_items[size].Focus();
+                m_items[size].Draw();
+            }
+            Await(m_items[0].Query, ESC);
+            Minimize();
+            Focus();
+        }
+
+        COORD GetOffset() {
+            short size = m_items.size();
+            COORD offset = {
+                m_position.X + m_orientation.X,
+                m_position.Y + m_orientation.Y + size
+            };
+            return offset
+        }
+
+        void Next() {
+            short next = m_selection + 1;
+            if (next < m_items.size())
+                SetSelection(next);
+        }
+
+        void Previous() {
+            short next = m_selection - 1;
+            if (next > 0)
+                SetSelection(next);
+        }
+
+        void SelectValue() {
+            Await(values.Select, ESC);
         }
 
     public:
-        std::vector<MenuItem>* Items = NULL;
-        Option* Parameters = NULL;
+        Option* values = NULL;
 
-        void Minimize() {
+        MenuItem& at(short item) { return m_items.at(item); }
+        MenuItem& operator[](short item) { return m_items[item]; }
 
+        void SetPosition(COORD* position) {
+            m_position = *position;
         }
 
-        
+        MenuItem* SetItems() {
+            m_command = Expand;
+            m_items = new std::vector<MenuItem>();
+            return this;
+        }
 
-        
-        void SetItems() {
-            
+        MenuItem* SetOrientation(bool vertical) {
+            m_orientation = vertical ? { 0, 1 } : { 1, 0 };
+            return this;
+        }
+
+        MenuItem* SetCommand(void (*command)()) {
+            m_command = command;
+            return this;
+        }
+
+        MenuItem* SetParameters(Option* parameters) {
+            values = parameters;
+            values->BindCaption(&m_caption);
+            m_command = SelectValue;
+            return this;
+        }
+
+        MenuItem* Add(MenuItem* item) {
+            COORD offset = GetOffset;
+            item.SetPosition(&offset);
+            m_items->push_back(*item);
+            return this;
+        }
+
+        void Draw() {
+            Pen::ink().Quote(m_caption);
+        }
+
+        void Clear() {
+            Pen::ink().screen->Clear()->Move();
         }
 
         void Focus() {
-            
+            Pen::ink().screen->Page(m_position.X);
+            Pen::ink().screen->Line(m_position.Y);
+            Pen::ink().screen->Move();
         }
 
-bool SelectMenuOption() {
-    bool is_exit = false;
+        char Query() {
+            char code = Select(menu_input_keys);
 
-    switch (current_menu_option) {
-        case 0:
-            current_menu_option = -1;
-            Await(ArrayMenuLoop(), ESC);
-            break;
-        case 1:
-            is_exit = Select(ESC, ENTER) == ENTER;
-            break;
-        default:
-            array_options[current_menu_option].Next();
-            break;
-    }
+            switch (code) {
+                case ENTER: m_items[m_selection].command(); break;
+                case KEY_UP:
+                case KEY_LEFT:
+                    Previous();
+                    break;
+                case KEY_DOWN:
+                case KEY_RIGHT:
+                    Next();
+                    break;
+                default:
+                    break;
+            }
 
-    return is_exit;
-}
-
-bool MenuLoop() {
-    char code = Select(menu_input_keys);
-    bool is_exit = false;
-
-    switch (code) {
-        case ENTER: is_exit = SelectMenuOption(); break;
-        case KEY_LEFT:
-            NavigateMenuOption(main_menu, -1);
-            break;
-        case KEY_RIGHT:
-            NavigateMenuOption(main_menu, 1);
-            break;
-        default: break;
-    };
-
-    return is_exit;
-}
-
+            return code;
+        }
 }
 
 #endif
